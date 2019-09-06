65beea6b9233e633cdc13b13c4a701bcb0e34364
Nick Pafundi <napafundi@gmail.com>
2019-09-06 19:11:17 -0400

    first commit, attempting to create and Android App version of the Java Monthly Goals application, setting up layouts currently

---
+++ app/src/main/java/com/napafundi/monthlygoals/MainActivity.java
@@ -0,0 +1,37 @@
+package com.napafundi.monthlygoals;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_goal:
+                final EditText taskEditText = new EditText(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
