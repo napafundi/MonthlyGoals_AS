88ec260944e6d34df99d9e79ee5f7ec77e183a27
Nick Pafundi <napafundi@gmail.com>
2019-09-14 21:50:10 -0400

    started addGoal function for AddGoalActivity, working on how I want to display invalid data entry

--- app/src/main/java/com/napafundi/monthlygoals/AddGoalActivity.java
+++ app/src/main/java/com/napafundi/monthlygoals/AddGoalActivity.java
@@ -2,8 +2,13 @@ package com.napafundi.monthlygoals;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
 
 public class AddGoalActivity extends AppCompatActivity {
 
@@ -17,7 +22,36 @@ public class AddGoalActivity extends AppCompatActivity {
         setContentView(R.layout.add_goal_form);
     }
 
+    /**
+     * Test goal title, date, and description to be non-empty and valid inputs before passing to
+     * data model
+     * @param view
+     */
+    public AlertDialog addGoal(View view) {
+        final EditText titleField = findViewById(R.id.EditTextMonthlyGoalTitle);
+        String title = titleField.getText().toString();
+
+        final DatePicker dateField = findViewById(R.id.DatePickerMonthlyGoalDate);
+        int day = dateField.getDayOfMonth();
+        int month = dateField.getMonth();
+        int year = dateField.getYear();
+        Calendar cal = Calendar.getInstance();
+        cal.set(year, month, day);
+
+        final EditText descField = findViewById(R.id.EditTextMonthlyGoalDescription);
+        String desc = descField.getText().toString();
+    }
+
     public void cancelAddGoal(View view) {
         finish();
+    }
+
+    private AlertDialog addGoalDialogBuilder(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage(message)
+                .setTitle(title);
+
+        return builder.create();
     }
 }
